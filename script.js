f2594d07f27dc3f19bb9cbc000beb4f60d978709
caio oliveira <caio2601@gmail.com>
2024-02-01 18:12:24 -0300

    button style commit

--- script.js
+++ script.js
@@ -1,43 +1,22 @@
-// const CVBtn = document.querySelector("#buttonCV");
+const CVBtn = document.querySelector(".btnCV");
 
-// CVBtn.addEventListener("click", () => {
-//     CVBtn.classList.add("onclick");
-//     setTimeout(() => {
-//         validade();
-//     }, 250);
-// });
+CVBtn.addEventListener('click', () => {
+    CVBtn.classList.add("onclick");
+    setTimeout(() => {
+        validade();
+    }, 250);
+});
 
-// function validade() {
-//     setTimeout(() => {
-//         CVBtn.classList.remove("onclick");
-//         CVBtn.classList.add("validade");
-//         setTimeout(callback, 450);
-//     }, 2250);
-// }
-
-// function callback() {
-//     setTimeout(() => {
-//         CVBtn.classList.remove("validade");
-//     }, 1250);
-// }
-
-$(function() {
-    $("#buttonCV").click(function() {
-        $("#buttonCV").addClass("onclic");
-        setTimeout(validate, 250);
-    });
-    
-    function validate() {
-        setTimeout(function() {
-        $("#buttonCV").removeClass("onclic");
-        $("#buttonCV").addClass("validate");
+function validade() {
+    setTimeout(() => {
+        CVBtn.classList.remove("onclick");
+        CVBtn.classList.add("validate");
         setTimeout(callback, 450);
-        }, 2250);
-    }
-    
-    function callback() {
-        setTimeout(function() {
-        $("#buttonCV").removeClass("validate");
-        }, 1250);
-    }
-    });
+    }, 2250);
+}
+
+function callback() {
+    setTimeout(() => {
+        CVBtn.classList.remove("validate");
+    }, 1250);
+}
